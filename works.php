f081c8450537e6a349a696ab0b7a969df88fee3b
abeller3 <beller.amanda@yahoo.com>
2015-05-06 23:31:59 -0500

    adjusted files to php to include master layout

---
+++ works.php
@@ -0,0 +1,32 @@
+<?php include 'masterindex.php' ?>
+<?php
+echo $header . $nav . $content_before;
+?>
+
+<div class="row">
+	<div class="large-3 columns">
+		<section class="box feature">
+			<a href="#" class="image featured"><img src="images/blueflowers.png" alt="" /></a>
+		</section>
+	</div>
+	<div class="large-3 columns">
+		<!-- Box -->
+		<section class="box feature">
+			<a href="#" class="image featured"><img src="images/redflowers.png" alt="" /></a>
+		</section>
+	</div>
+	<div class="large-3 columns">
+		<!-- Box -->
+		<section class="box feature last">
+			<a href="#" class="image featured"><img src="images/humbirdparty.png" alt="" /></a>
+		</section>
+	</div>
+	<div class="large-3 columns">
+		<!-- Box -->
+		<section class="box feature last">
+			<a href="#" class="image featured"><img src="images/humbirdparty.png" alt="" /></a>
+		</section>
+	</div>
+</div>
+
+<?php echo $content_after; ?>
---
+++ workszoom.php
@@ -0,0 +1,12 @@
+<?php include 'masterindex.php' ?>
+<?php
+echo $header . $nav . $content_before;
+?>
+<div class="row">
+	<div class="large-12 med-12 small-12 columns">
+		<section class="box feature">
+			<a href="#" class="image featured"><img src="images/blueflowers.png" alt="" /></a>
+		</section>
+	</div>
+</div>
+<?php echo $content_after; ?>
