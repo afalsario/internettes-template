f6201507c91609e60a61cbb9bd344de753e4833a
abeller3 <beller.amanda@yahoo.com>
2015-05-09 23:01:58 -0500

    changed button color

--- collections.php
+++ collections.php
@@ -2,7 +2,7 @@
 <?php
 echo $header . $nav . $content_before;
 ?>
-	<div class="row">
+	<div class="row recent-work">
 		<h1>Portfolio of Works</h1>
 		<div class="large-6 columns">
 			<a target="_blank" href="birdflower.png">
@@ -10,7 +10,7 @@ echo $header . $nav . $content_before;
 			</a>
 			<div class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 				<div class="center_button">
-					<input type="submit" class="button" value="Submit">
+					<input type="submit" class="button" value="View Collection">
 				</div>
 			</div>
 		</div>
@@ -20,7 +20,7 @@ echo $header . $nav . $content_before;
 			</a>
 			<div class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
 				<div class="center_button">
-					<input type="submit" class="button" value="Submit">
+					<input type="submit" class="button" value="View Collection">
 				</div>
 			</div>
 		</div>
@@ -37,7 +37,7 @@ echo $header . $nav . $content_before;
 			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 			consequat. 
 			<div class="center_button">
-				<input type="submit" class="button" value="Submit">
+				<input type="submit" class="button" value="View Collection">
 			</div>
 		</div>
 
@@ -51,7 +51,7 @@ echo $header . $nav . $content_before;
 			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 			consequat.
 			<div class="center_button">
-				<input type="submit" class="button" value="Submit">
+				<input type="submit" class="button" value="View Collection">
 			</div>
 		</div>
 	</div>
