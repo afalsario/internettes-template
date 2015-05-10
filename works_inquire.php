145d6b1e24caa9c5aae4af2c142f1854aa45a668
Ashley <a_falsario@yahoo.com>
2015-05-09 21:41:05 -0500

    fixing margins on works inquire

--- works_inquire.php
+++ works_inquire.php
@@ -14,16 +14,16 @@ echo $header . $nav . $content_before;
     <form method="post" action="process.php">
         <label for="name">Name: <span class="required">*</span></label>
         <input type="text" id="name" name="name" value="" placeholder="John Doe" required="required" autofocus="autofocus" />
-         
+
         <label for="email">Email Address: <span class="required">*</span></label>
         <input type="email" id="email" name="email" value="" placeholder="johndoe@example.com" required="required" />
-         
+
         <label for="telephone">Telephone: </label>
         <input type="tel" id="telephone" name="telephone" value="" />
-         
+
         <label for="message">Message: <span class="required">*</span></label>
         <textarea id="message" name="message" placeholder="Your message must be greater than 20 charcters" required="required" data-minlength="20"></textarea>
-         
+
         <span id="loading"></span>
         <input type="submit" value="Submit" id="submit-button" />
         <p id="req-field-desc"><span class="required">*</span> indicates a required field</p>
