b2885fc9cae9e52ca00ac31c00b20182d6717808
Ashley <a_falsario@yahoo.com>
2015-05-11 15:20:45 -0500

    creating masonry display for works.php

--- works.php
+++ works.php
@@ -2,65 +2,36 @@
 <?php
 echo $header . $nav . $content_before;
 ?>
-<div class="row recent-work">
-            <h1 class="small-12 large-centered columns">Nature's Beauty</h1>
-            <div class="large-4 medium-4 columns">
-                <section class="box feature">
-                    <a href="#" class="image featured"><img src="images/birdflower.png" alt="" /></a>
-                </section>
+        <h1 class="small-12 large-centered columns">Nature's Beauty</h1>
+            <div id="container" class="cols">
+                <div class="box">
+                    <a href="#"><img src="images/birdflower.png" alt="" /></a>
+                </div>
+                <div class="box">
+                    <a href="#"><img src="images/darksunflower.png" alt="" /></a>
+                </div>
+                <div class="box">
+                    <a href="#"><img src="images/humbirdparty.png" alt="" /></a>
+                </div>
+                <div class="box">
+                    <a href="#"><img src="images/blueflowers.png" alt="" /></a>
+                </div>
+                <div class="box">
+                    <a href="#"><img src="images/stars.png" alt="" /></a>
+                </div>
+                <div class="box">
+                    <a href="#"><img src="images/darksunflower.png" alt="" /></a>
+                </div>
+                <div class="box">
+                    <a href="#"><img src="images/birdflower.png" alt="" /></a>
+                </div>
+                <div class="box">
+                    <a href="#"><img src="images/darksunflower.png" alt="" /></a>
+                </div>
+                <div class="box">
+                    <a href="#"><img src="images/humbirdparty.png" alt="" /></a>
+                </div>
             </div>
-            <div class="large-4 medium-4 columns">
-                <!-- Box -->
-                <section class="box feature">
-                    <a href="#" class="image featured"><img src="images/darksunflower.png" alt="" /></a>
-                </section>
-            </div>
-            <div class="large-4 medium-4 columns">
-                <!-- Box -->
-                <section class="box feature last">
-                    <a href="#" class="image featured"><img src="images/humbirdparty.png" alt="" /></a>
-                </section>
-            </div>
-        </div>
-        <div class="row recent-work">
-            <div class="large-4 medium-4 columns">
-                <section class="box feature">
-                    <a href="#" class="image featured"><img src="images/blueflowers.png" alt="" /></a>
-                </section>
-            </div>
-            <div class="large-4 medium-4 columns">
-                <!-- Box -->
-                <section class="box feature">
-                   <a href="#" class="image featured"><img src="images/stars.png" alt="" /></a>
-                </section>
-            </div>
-            <div class="large-4 medium-4 columns">
-                <!-- Box -->
-                <section class="box feature last">
-                    <a href="#" class="image featured"><img src="images/darksunflower.png" alt="" /></a>
-                </section>
-            </div>
-        </div>
-        <div class="row recent-work">
-            <div class="large-4 medium-4 columns">
-                <section class="box feature">
-                    <a href="#" class="image featured"><img src="images/birdflower.png" alt="" /></a>
-                </section>
-            </div>
-            <div class="large-4 medium-4 columns">
-                <!-- Box -->
-                <section class="box feature">
-                    <a href="#" class="image featured"><img src="images/darksunflower.png" alt="" /></a>
-                </section>
-            </div>
-            <div class="large-4 medium-4 columns">
-                <!-- Box -->
-                <section class="box feature last">
-                    <a href="#" class="image featured"><img src="images/humbirdparty.png" alt="" /></a>
-                </section>
-            </div>
-        </div>
-
 
 <?php echo $content_after; ?>
 <br>
