17d71989f9230c2e15325d33ce157270cfda7af4
abeller3 <beller.amanda@yahoo.com>
2015-05-11 14:38:31 -0500

    adjusted menu and css

--- workszoom.php
+++ workszoom.php
@@ -6,16 +6,18 @@ echo $header . $nav . $content_before;
 	<div class=" featured-image large-12 med-12 small-12 columns">
 		<section>
 			<a href="#" class="image featured"><img src="images/blueflowers.png" alt="" /></a>
-			<h2 class="left-align">Blue Flowers</h2>
-			<p>30 X 30</p>
-			<p>$900 USD</p>
-			<p>Availability: On Hold</p>
 		</section>
 	</div>
 </div>
  <!--Gallery Info -->
 <div class="row">
-	<div class="large-12 medium-12 small-12 columns">
+    <div class="large-6 medium-6 small-12 columns">
+        <h2 class="left-align">Blue Flowers</h2>
+        <p>30 X 30</p>
+        <p>$900 USD</p>
+        <p>Availability: On Hold</p>
+    </div>
+	<div class="large-6 medium-6 small-12 columns">
 		<section class="box">
 			<h2 class="left-align">Gallery Info</h2>
         <h4 class="zoom_left">Interesting Fine Art Gallery</h4>
@@ -24,8 +26,11 @@ echo $header . $nav . $content_before;
             Anchorage, Alaska 99501
         </p>
         <a href="#"><button class="button center">More Details</button></a>
-        </section>
+
     </div>
+        
+        </section>
+
 </div>
 
 
@@ -35,7 +40,8 @@ echo $header . $nav . $content_before;
 <div class="cta">
         <div class="row">
             <h1 class="small-12 columns">Interested?</h1>
-            <a href="#"><button class="button">Contact Artist</button></a>
+            <a href="works_inquire.php"><button class="button">Contact Artist</button></a>
+                    <a href="#"><button class="Paypal"><img src="https://www.paypalobjects.com/webstatic/en_US/i/buttons/buy-logo-large.png" alt="Buy now with PayPal" /></button></a>
 
         </div>
     </div>
