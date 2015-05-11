7f29cabb5e6218965ed5bafef1817c2225809741
abeller3 <beller.amanda@yahoo.com>
2015-05-10 23:28:14 -0500

    changes to workszoom css

--- workszoom.php
+++ workszoom.php
@@ -3,11 +3,40 @@
 echo $header . $nav . $content_before;
 ?>
 <div class="row">
-	<div class="large-12 med-12 small-12 columns">
-		<section class="box feature">
+	<div class=" featured-image large-12 med-12 small-12 columns">
+		<section>
 			<a href="#" class="image featured"><img src="images/blueflowers.png" alt="" /></a>
+			<h2 class="left-align">Blue Flowers</h2>
+			<p>30 X 30</p>
+			<p>$900 USD</p>
+			<p>Availability: On Hold</p>
 		</section>
 	</div>
 </div>
+ <!--Gallery Info -->
+<div class="row">
+	<div class="large-12 medium-12 small-12 columns">
+		<section class="box">
+			<h2 class="left-align">Gallery Info</h2>
+        <h4 class="zoom_left">Interesting Fine Art Gallery</h4>
+        <p>(907) 903-9997<br>
+            4th Avenue Marketplace, 333 W. 4th Ave, #211<br>
+            Anchorage, Alaska 99501
+        </p>
+        <a href="#"><button class="button center">More Details</button></a>
+        </section>
+    </div>
+</div>
+
+
+
 <?php echo $content_after; ?>
+
+<div class="cta">
+        <div class="row">
+            <h1 class="small-12 columns">Interested?</h1>
+            <a href="#"><button class="button">Contact Artist</button></a>
+
+        </div>
+    </div>
 <?php echo $footer; ?>
