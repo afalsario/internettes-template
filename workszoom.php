f3105ffe79626ca644a7831c7d8d747d3c6e16d9
abeller3 <beller.amanda@yahoo.com>
2015-05-11 19:39:47 -0500

    updating workzoom

--- workszoom.php
+++ workszoom.php
@@ -3,7 +3,6 @@
 echo $header . $nav . $content_before;
 ?>
 <div class="row">
-<<<<<<< Updated upstream
     <div class=" featured-image large-12 med-12 small-12 columns">
         <section>
             <a href="#" class="image featured"><img src="images/blueflowers.png" alt="" /></a>
@@ -30,36 +29,6 @@ echo $header . $nav . $content_before;
         </p>
         <a href="#"><button class="button center">More Details</button></a>
     </div> 
-=======
-	<div class=" featured-image large-12 med-12 small-12 columns">
-		<section>
-			<a href="#" class="image featured"><img src="images/blueflowers.png" alt="" /></a>
-			<div class="large-12 medium-12 small-12 columns">
-                <h2 class="left-align">Gallery Info</h2>
-        <h4 class="zoom_left">Interesting Fine Art Gallery</h4>
-        <p>(907) 903-9997<br>
-            4th Avenue Marketplace, 333 W. 4th Ave, #211<br>
-            Anchorage, Alaska 99501
-        </p>
-        <a href="#"><button class="button center">More Details</button></a>
-
-            </div>
-
-            <h2 class="left-align">Blue Flowers</h2>
-			<p>30 X 30</p>
-			<p>$900 USD</p>
-			<p>Availability: On Hold</p>
-		</section>
-	</div>
-</div>
- <!--Gallery Info -->
-<div class="row">
-	<div class="large-12 medium-12 small-12 columns">
-		<section class="box">
-			
->>>>>>> Stashed changes
-        </section>
-
 </div>
 
 
