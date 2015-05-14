aa317e65915714b6185c8ebdff14ec2324c85f13
abeller3 <beller.amanda@yahoo.com>
2015-05-13 20:06:36 -0500

    creating workshops page and css for page

---
+++ workshops.php
@@ -0,0 +1,19 @@
+<?php include 'masterindex.php' ?>
+<?php
+echo $header . $nav . $content_before;
+?> 
+<div class="wrapper">
+	<div class="row">
+		<div class="large-4 small-6 columns">
+			<img class="image-blog" src="images/paint.png" alt=""/>
+		</div>
+		<div class="large-8 small-16 columns">
+			<h1 class="left-align">Learn The Art of The Flower</h1>
+			<p>Dates: 07/07/2015-08/22/2015</p>
+			<p>Location: 1234 Garden Lane</p>
+		</div>
+	</div>
+</div>
+
+<?php echo $content_after; ?>
+<?php echo $footer; ?>
