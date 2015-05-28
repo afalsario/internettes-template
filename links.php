7cf5d13a55c2eb16710221e16af81d51314322b5
Ashley <a_falsario@yahoo.com>
2015-05-27 21:18:52 -0500

    adding links page

--- links.php
+++ links.php
@@ -4,13 +4,13 @@ echo $header . $nav . $content_before;
 ?>
 
 <h1>My Links</h1>
-
-<div class="row">
-    <div class="large-12 columns">
-        <a href="#">Google</a>
-        <a href="#">Yahoo</a>
-    </div>
+<div class="page-list large-8 large-centered columns">
+    <a href="http://faso.com/">Link 1</a>
+    <a href="http://faso.com/">Link 2</a>
+    <a href="http://faso.com/">Link 3</a>
 </div>
+
+
 
 
 <?php echo $content_after; ?>
