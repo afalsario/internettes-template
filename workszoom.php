e2ddcac90c62b6eac09c5f40a0056d933b199e45
abeller3 <beller.amanda@yahoo.com>
2015-05-27 21:56:08 -0500

    changes to cta bar

--- workszoom.php
+++ workszoom.php
@@ -3,31 +3,29 @@
 echo $header . $nav . $content_before;
 ?>
 
+<!-- Pagination -->
 <div class="row recent-work">
-            <div class="large-12 medium-12 columns">
-                <ul class="pagination" role="menubar" aria-label="Pagination">
-  <li class="arrow unavailable" aria-disabled="true"><a href="">&laquo; Previous</a></li>
-  <li class="current"><a href="">1</a></li>
-  <li><a href="">2</a></li>
-  <li><a href="">3</a></li>
-  <li><a href="">4</a></li>
-  <li class="unavailable" aria-disabled="true"><a href="">&hellip;</a></li>
-  <li><a href="">12</a></li>
-  <li><a href="">13</a></li>
-  <li class="arrow"><a href="">Next &raquo;</a></li>
-</ul>
-            <br></br>
-            <br></br>
+    <div class="large-12 medium-12 columns">
+        <ul class="pagination" role="menubar" aria-label="Pagination">
+            <li class="arrow unavailable" aria-disabled="true"><a href="">&laquo; Previous</a></li>
+            <li class="current"><a href="">1</a></li>
+            <li><a href="">2</a></li>
+            <li><a href="">3</a></li>
+            <li><a href="">4</a></li>
+            <li class="unavailable" aria-disabled="true"><a href="">&hellip;</a></li>
+            <li><a href="">12</a></li>
+            <li><a href="">13</a></li>
+            <li class="arrow"><a href="">Next &raquo;</a></li>
+        </ul>
+    </div>
+</div>
 
-            </div>
-        </div>
+<!--Featured Image -->
+
 <div class="row">
     <div class=" featured-image large-12 med-12 small-12 columns">
-        <section>
-            <a href="#" class="image featured"><img src="images/blueflowers.png" alt="" /></a>
-        </section>
+        <a href="#" class="image featured"><img src="images/blueflowers.png" alt="" /></a>
     </div>
-
 </div>
 
 <!--Gallery Info -->
@@ -46,12 +44,10 @@ echo $header . $nav . $content_before;
             <h4 class="zoom_left">Interesting Fine Art Gallery</h4>
             <p>(907) 903-9997<br>
             4th Avenue Marketplace, 333 W. 4th Ave, #211<br>
-            Anchorage, Alaska 99501
-         </p>
-        <a href="#"><button class="button center">More Details</button></a>
-    </div> 
-
-</div>
+            Anchorage, Alaska 99501</p>
+            <a href="#"><button class="button center">More Details</button></a>
+        </div>
+    </div>
 <br>
 
 <?php echo $content_after; ?>
